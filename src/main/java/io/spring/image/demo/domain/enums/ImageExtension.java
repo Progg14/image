458dec99181a13a9ec8eef295ab761789457ac16
Progg14 <martins.joao@estudante.ifmt.edu.br>
2026-05-22 11:59:04 -0400

    Add files via upload

--- src/main/java/io/spring/image/demo/domain/enums/ImageExtension.java
+++ src/main/java/io/spring/image/demo/domain/enums/ImageExtension.java
@@ -23,5 +23,11 @@ public enum ImageExtension {
         return Arrays.stream(values())
                 .filter(ie-> ie.mediaType.equals(mediaType)).findFirst().orElse(null);
     }
+    public static ImageExtension ofName(String name){
+        return Arrays.stream(values())
+                .filter(ie-> ie.name().equals(name))
+                .findFirst()
+                .orElse(null);
+    }
 
 }
